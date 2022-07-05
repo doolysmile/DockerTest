658f784550c7da61f4daf0b0d9635ceeb23ebfa1
KCH <doolysmile1@naver.com>
2022-07-05 22:09:39 +0900

    Initial commit

---
+++ src/test/java/com/example/kch/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kch.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
